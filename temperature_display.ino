e5728a3a6cea488514fb4fc8658c8d80ab63b654
Henare Degan <henare.degan@gmail.com>
2018-08-26 11:37:06 +1000

    Display temperature from DHT22 sensor

--- temperature_display.ino
+++ temperature_display.ino
@@ -1,5 +1,4 @@
 #include "SSD1306Spi.h"
-
 // Initialize the OLED display using SPI
 // D5 -> CLK (Display D0)
 // D7 -> MOSI (DOUT) (Display D1)
@@ -8,30 +7,57 @@
 // D8 -> CS
 SSD1306Spi        display(D0, D2, D8);
 
+#include "DHT.h"
+#define DHTPIN D6
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("starting...");
+  Serial.println("Starting setup...");
   Serial.println();
+
+  dht.begin();
 
   // Initialising the UI will init the display too.
   display.init();
-
+  // Displays the yellow bar on the top
   display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_10);
-
-  display.clear();
-  drawTemperature();
-  display.display();
-}
-
-void drawTemperature() {
-    display.setTextAlignment(TEXT_ALIGN_CENTER);
-    display.setFont(ArialMT_Plain_16);
-    display.drawString(64, 0, "Inside");
-    display.setFont(ArialMT_Plain_24);
-    display.drawString(64, 16, "24.6°");
 }
 
 void loop() {
-//  delay(10);
+  // Run every 10 seconds
+  delay(10000);
+
+  // Get temperature
+  float temperature = dht.readTemperature();
+  // Checks for failure and retries
+  if (isnan(temperature)) {
+    Serial.println("Failed to read temperature.");
+    return;
+  }
+
+  // Output display
+  display.clear();
+
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(64, 0, "Inside");
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(64, 16, Float2String(temperature, 1) + "°");
+
+  display.display();
+}
+
+/*****************************************************************
+/* convert float to string                                       *
+/*****************************************************************/
+String Float2String(const double value, unsigned int digits) {
+  char temp[15];
+  String s;
+
+  dtostrf(value, 13, digits, temp);
+  s = String(temp);
+  s.trim();
+  return s;
 }
